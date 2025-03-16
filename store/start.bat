0417682117b671ddfd3c2050a4ff68d079d0ecfd
lutzpeschlow <88546307+lutzpeschlow@users.noreply.github.com>
2025-03-16 19:08:47 +0100

    Delete store directory

--- store/start.bat
+++
@@ -1,6 +0,0 @@
-REM    input_attributes.txt contains the command and keywords to search ....
-REM C:/soft/python/V3_7_9/python.exe c:/tmp/python/tpl_search/git_folder/tpl_search-1/tpl_search.py --search "SOL CQUAD4 FORCE" --not "FLUID"
-REM C:/soft/python/V3_7_9/python.exe c:/tmp/python/tpl_search/git_folder/tpl_search-1/tpl_search.py --create --sql_path_loc="C:\tmp\python\tpl_search\example_folders"
-REM C:/soft/python/V3_7_9/python.exe c:/tmp/python/tpl_search/git_folder/tpl_search-1/tpl_search.py --search "SOL CQUAD4 FORCE" --not "FLUID" --sql_path_loc="C:\tmp\python\tpl_search\example_folders"
-C:/soft/python/V3_7_9/python.exe c:/tmp/python/tpl_search/git_folder/tpl_search-1/tpl_search.py
-PAUSE
